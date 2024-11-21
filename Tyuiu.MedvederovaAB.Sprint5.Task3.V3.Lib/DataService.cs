af8799d90b8036966ebc60d67a382a0e3cf634ab
Амина Медведерова <medvederovaamina@outlook.com>
2024-11-21 11:56:18 +0500

    Task3

---
+++ Tyuiu.MedvederovaAB.Sprint5.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task3.V3.Lib
+{
+    public class DataService : ISprint5Task3V3
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string[] paths = { Path.GetTempPath(), Path.GetTempFileName() };
+            string path = Path.Combine(paths);
+
+            double result = Math.Round(x * Math.Sqrt(x + 3), 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(result));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MedvederovaAB.Sprint5.Task3.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MedvederovaAB.Sprint5.Task3.V3.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            Console.Title = "Спринт #5  |  Выполнила: Медведерова А. Б.   |  ПКТб-24-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #5                                                     *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл.          *");
+            Console.WriteLine("* Задание #3                                                    *");
+            Console.WriteLine("* Вариант #3                                                    *");
+            Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКТб-24-1            *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      *");
+            Console.WriteLine("* Написать программу, которая вычисляет выпажение по формуле    *");
+            Console.WriteLine("* при х = 3.                                                    *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine("*****************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл" + res);
+            Console.WriteLine("Создан!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
