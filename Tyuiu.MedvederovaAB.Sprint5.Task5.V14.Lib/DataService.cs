439095c90dea5d54ece3199c32a8dfbe02f991f7
Амина Медведерова <medvederovaamina@outlook.com>
2024-11-23 22:47:02 +0500

    Task5

---
+++ Tyuiu.MedvederovaAB.Sprint5.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task5.V14.Lib
+{
+    public class DataService : ISprint5Task5V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double min = double.MaxValue;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(' ');
+                    foreach (string number in numbers)
+                    {
+                        double number_ = double.Parse(number, CultureInfo.InvariantCulture);
+                        if (number_ % 3.0 == 0 && number_ < min)
+                        {
+                            min = number_;
+                        }
+                    }
+                }
+            }
+            return Math.Round(min, 3);
+        }
+    }
+}
---
+++ Tyuiu.MedvederovaAB.Sprint5.Task5.V14/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.MedvederovaAB.Sprint5.Task5.V14.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task5.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5  |  Выполнила: Медведерова А. Б.   |  ПКТб-24-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #5                                                     *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                      *");
+            Console.WriteLine("* Задание #5                                                    *");
+            Console.WriteLine("* Вариант #14                                                   *");
+            Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКТб-24-1            *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение по формуле    *");
+            Console.WriteLine("* при значении х , взятого из файла.                            *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine("*****************************************************************");
+
+            string path = @"C:\Users\Амина Медведерова\source\repos\Tyuiu.MedvederovaAB.Sprint5\Tyuiu.MedvederovaAB.Sprint5.Task5.V14\bin\Debug\net8.0\InPutDataFileTask5V14.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
