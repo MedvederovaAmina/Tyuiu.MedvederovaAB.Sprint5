ff02637da5ff03b98c7e1a2cac899796945b6148
Амина Медведерова <medvederovaamina@outlook.com>
2024-11-26 23:16:28 +0500

    Task6

---
+++ Tyuiu.MedvederovaAB.Sprint5.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task6.V16.Lib
+{
+    public class DataService : ISprint5Task6V16
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            string str = File.ReadAllText(@"C:\Users\Амина Медведерова\source\repos\Tyuiu.MedvederovaAB.Sprint5\Tyuiu.MedvederovaAB.Sprint5.Task6.V16\bin\Debug\net8.0\InPutDataFileTask6V16.txt");
+            string res = "";
+            foreach (char c in str)
+            {
+                if (char.IsLower(c) && (Regex.IsMatch(Convert.ToString(c), @"\p{IsBasicLatin}")))
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        } 
+    }
+}
+        
---
+++ Tyuiu.MedvederovaAB.Sprint5.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+using Tyuiu.MedvederovaAB.Sprint5.Task6.V16.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint5.Task6.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Амина Медведерова\source\repos\Tyuiu.MedvederovaAB.Sprint5\Tyuiu.MedvederovaAB.Sprint5.Task6.V16\bin\Debug\net8.0\InPutDataFileTask6V16.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void LoadFromDataFile()
+        {
+            string path = @"C:\Users\Амина Медведерова\source\repos\Tyuiu.MedvederovaAB.Sprint5\Tyuiu.MedvederovaAB.Sprint5.Task6.V16\bin\Debug\net8.0\InPutDataFileTask6V16.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
